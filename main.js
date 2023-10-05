cba264f3ca770f7630da6953675eeccb68b3346b
natidal <130075226+natidal@users.noreply.github.com>
2023-10-05 09:08:28 -0300

    05/10/2023

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 alert('auau'):
+
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+function tocaSomPom(){ document.querySelector} ( '#som_tecla_pom').play();
+function tocaSomPom(){ document.querySelector ( '#som_tecla_pom').play();
+}
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
+function tocaSomTim(){ document.querySelector ( '#som_tecla_tim').play();
+}
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+function tocaSomPuff(){ document.querySelector ( '#som_tecla_Puff').play();
+}
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+function tocaSomsplash(){ document.querySelector ( '#som_tecla_Splash').play();
+}
+document.querySelector('.tecla_toin').onclick = tocaSomToin;
+function tocaSomToin(){ document.querySelector ( '#som_tecla_Toin').play();
+}
+document.querySelector('.tecla_psh').onclick = tocaSomPsh;
+function tocaSomPsh(){ document.querySelector ( '#som_tecla_Psh').play();
+}
+document.querySelector('.tecla_tic').onclick = tocaSomTic;
+function tocaSomTic(){ document.querySelector ( '#som_tecla_Tic').play();
+}
+document.querySelector('.tecla_tom').onclick = tocaSomTom;
+function tocaSomTom(){ document.querySelector ( '#som_tecla_Tom').play();
+}
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+function tocaSomClap(){ document.querySelector ( '#som_tecla_Clap').play();
+}
